00693481e25a9d05486febc3713eba3117b88ff8
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-07-08 21:50:41 +0300

    Update RP.js

--- RP.js
+++ RP.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client( { autoreconnect: true } );
 
 client.on('ready', () => {
     console.log("Успешный запуск RP");
