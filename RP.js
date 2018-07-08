8ab4530e08bed2c278109df46c86fc6eb2ae1cd5
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-07-08 21:27:07 +0300

    Create RP.js

---
+++ RP.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("Успешный запуск RP");
+    client.user.setPresence({
+      status: 'dnd', //статус вашего аккаунта (dnd, idle, invisible, online)
+      game: {
+        name: 'cosmoteer', //название вашей игры
+        state: 'in game', //текст на 2 линии
+        details: '12 solar systems', //текст на 1 линии
+        application_id: '442351930929577995', //id вашего приложения
+        assets: { //ассесты
+          small_image: '465138149413224483', //id от маленько ассеста
+          small_text: 'X-49', //текст который будет виден при наведение на маленький ассест
+          large_image: '465138072498208768', //id от большого ассеста
+          large_text: 'Rocket Shark' //текст который будет виден при наведение на большой ассест
+        }
+      }
+    },);
+    console.log("Успех");
+});
+
+client.login(process.env.TOKEN)
